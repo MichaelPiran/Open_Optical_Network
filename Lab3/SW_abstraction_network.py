66e8ade581a9c5f409f8fdc8ac97a291292dcfc8
MichaelPiran <s269935@studenti.polito.it>
2020-11-13 18:12:32 +0100

    End ex1

--- Lab3/SW_abstraction_network.py
+++ Lab3/SW_abstraction_network.py
@@ -1,17 +1,38 @@
-class Signal_information:
-    def __init__(self):
-        self.signal_power = 3.0
-        self.noise_power = 0.0
-        self.latency = 0.0
-        self.path = []
+class Signal_information(object):
+    def __init__(self, power, path):
+        self._signal_power = power
+        self._noise_power = 0.0
+        self._latency = 0.0
+        self._path = path #[A, B, C, D, E, F]
     @property
-    def age(self):
-
-    @age.setter
-    def age(self,a):
+    def signal_power(self):
+        return self._signal_power
+    @property
+    def path(self):
+        return self._path
+    @path.setter
+    def path(self, path):
+        self._path = path
+    @property
+    def noise_power(self):
+        return self._noise_power
+    @noise_power.setter
+    def noise_power(self, noise):
+        self._noise_power = noise
+    @property
+    def latency(self):
+        return self._latency
+    @latency.setter
+    def latency(self, latency):
+        self._latency = latency
+    def add_noise(self, noise):
+        self.noise_power += noise
+    def add_latency(self, latency):
+        self.latency += latency
+    def next(self):
+        self.path = self.path [1:]
 def main():
-
-  #error
+    #error
 
 if __name__ == "__main__":
     main()
