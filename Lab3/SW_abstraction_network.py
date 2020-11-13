013f2ded3b185cdb595fb8b2acbad6a099394283
MichaelPiran <s269935@studenti.polito.it>
2020-11-13 17:42:46 +0100

    First commit lab3

---
+++ Lab3/SW_abstraction_network.py
@@ -0,0 +1,17 @@
+class Signal_information:
+    def __init__(self):
+        self.signal_power = 3.0
+        self.noise_power = 0.0
+        self.latency = 0.0
+        self.path = []
+    @property
+    def age(self):
+
+    @age.setter
+    def age(self,a):
+def main():
+
+  #error
+
+if __name__ == "__main__":
+    main()
