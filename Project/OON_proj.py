12be20a19fd28b4a329dbc823e962a71883aa0de
MichaelPiran <s269935@studenti.polito.it>
2020-11-25 18:40:42 +0100

    End Lab4

--- Project/OON_proj.py
+++ Project/OON_proj.py
@@ -102,6 +102,7 @@ class Line(object):
         self._label = line_dict['label']
         self._length = line_dict['length']
         self._successive = {}
+        self._state = 'free'  # free or occupied
 
     @property
     def label(self):
@@ -114,6 +115,14 @@ class Line(object):
     @property
     def successive(self):
         return self._successive
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        self._state = state
 
     def latency_generation(self):
         latency = self.length / (c * 2 / 3)
@@ -237,8 +246,14 @@ class Network(object):
         for path in Network.find_paths(self, i_node.label, o_node.label):  # Retrieve all paths
             for row in self._weighted_paths.itertuples():  # Search inside the dataframe
                 if row.path == path:
-                    path_list.append(row.path)
-                    snr_list.append(row.snr)
+                    # If the line between each node is occupied don't consider it
+                    flag_state = 'free'
+                    for i in range(len(row.path)-1):
+                        if self._lines[row.path[i]+row.path[i+1]].state == 'false':
+                            flag_state = 'false'
+                    if flag_state == 'free':
+                        path_list.append(row.path)
+                        snr_list.append(row.snr)
         max_path = path_list[snr_list.index(max(snr_list))]
         return max_path
 
@@ -248,24 +263,34 @@ class Network(object):
         for path in Network.find_paths(self, i_node.label, o_node.label):  # Retrieve all paths
             for row in self._weighted_paths.itertuples():  # Search inside the dataframe
                 if row.path == path:
-                    path_list.append(row.path)
-                    lat_list.append(row.latency)
+                    # If the line between each node is occupied don't consider it
+                    flag_state = 'free'
+                    for i in range(len(row.path)-1):
+                        if self._lines[row.path[i]+row.path[i+1]].state == 'false':
+                            flag_state = 'false'
+                    if flag_state == 'free':
+                        path_list.append(row.path)
+                        lat_list.append(row.latency)
         min_path = path_list[lat_list.index(min(lat_list))]
         return min_path
 
     def stream(self, conn_list, lat_snr_label):
         path = []
-        for elem in conn_list:  # CHeck all connection istance
+        for elem in conn_list:  # Check all connection istance
             if lat_snr_label == 'latency':  # If check for latency
                 path = Network.find_best_latency(self, self._nodes[elem.input], self._nodes[elem.output])
             elif lat_snr_label == 'snr':  # If check for snr
                 path = Network.find_best_snr(self, self._nodes[elem.input], self._nodes[elem.output])
-            signal_information = SignalInformation(1, path)
-            signal_information = Network.propagate(self, signal_information)
-            if lat_snr_label == 'latency':  # If check for latency
-                elem.latency = signal_information.latency
-            elif lat_snr_label == 'snr':
-                elem.snr = 10 * np.log10(signal_information.signal_power / signal_information.noise_power)
+            if len(path) != 0 :  # There is almost a free path available
+                signal_information = SignalInformation(1, path)
+                signal_information = Network.propagate(self, signal_information)
+                if lat_snr_label == 'latency':  # If check for latency
+                    elem.latency = signal_information.latency
+                elif lat_snr_label == 'snr':
+                    elem.snr = 10 * np.log10(signal_information.signal_power / signal_information.noise_power)
+            else:
+                elem.latency = None
+                elem.snr = 0
 
 
 #############################################################
@@ -351,44 +376,50 @@ def main():
 
     # Es1 lab4
     network.set_weighted_paths(n_paths, latencies, noises, snrs)  # Instance weighted graph
+
     # Es2 lab4
     # snr_custom_nodes = network.find_best_snr(network.nodes['C'], network.nodes['A'])  # Search for path with bst snr
     # print('Path with best snr between the two input nodes is: ', snr_custom_nodes)
+
     # Es3 lab4
     # lat_custom_nodes = network.find_best_latency(network.nodes['C'], network.nodes['A'])  # Search bst snr
     # print('Path with best latency between the two input nodes is: ', lat_custom_nodes)
     # Es5 lab4
+
     # con1 = Connection({'input': 'E', 'output': 'B', 'signal_power': 1})
     # con2 = Connection({'input': 'B', 'output': 'D', 'signal_power': 1})
     # con3 = Connection({'input': 'C', 'output': 'A', 'signal_power': 1})
     # con4 = Connection({'input': 'A', 'output': 'C', 'signal_power': 1})
     # con_dict = [con1, con2, con3, con4]
     # network.stream(con_dict, 'snr')   # Latency or snr
+
     # Es6 lab4
-    con_dict = []
-    for i in range(100):
-        i_node = random.choice(list(network.nodes))  # Random input node
-        o_node = random.choice(list(network.nodes))  # Random output node
-        while i_node == o_node:   # Output node must be different from input ones
-            o_node = random.choice(list(network.nodes))
-        con_dict.append(Connection({'input': i_node, 'output': o_node, 'signal_power': 1}))
-    network.stream(con_dict, 'latency')
-    network.stream(con_dict, 'snr')
-    lbl_axes = []
-    lat_axes = []
-    snr_axes = []
-    for i in range(100):
-        lbl_axes.append(con_dict[i].input + con_dict[i].output)
-        lat_axes.append(con_dict[i].latency)
-        snr_axes.append(con_dict[i].snr)
-    plt.figure(figsize=(9, 3))
-    plt.subplot(121)
-    plt.bar(lbl_axes, lat_axes)  # latency distribution
-    plt.ylabel('latency')
-    plt.subplot(122)
-    plt.bar(lbl_axes, snr_axes)  # snr distribution
-    plt.ylabel('snr')
-    plt.show()
+    # con_dict = []
+    # for i in range(100):
+    #     i_node = random.choice(list(network.nodes))  # Random input node
+    #     o_node = random.choice(list(network.nodes))  # Random output node
+    #     while i_node == o_node:   # Output node must be different from input ones
+    #         o_node = random.choice(list(network.nodes))
+    #     con_dict.append(Connection({'input': i_node, 'output': o_node, 'signal_power': 1}))
+    # network.stream(con_dict, 'latency')
+    # network.stream(con_dict, 'snr')
+    # lbl_axes = []
+    # lat_axes = []
+    # snr_axes = []
+    # for i in range(100):
+    #     lbl_axes.append(con_dict[i].input + con_dict[i].output)
+    #     lat_axes.append(con_dict[i].latency)
+    #     snr_axes.append(con_dict[i].snr)
+    # plt.figure(figsize=(9, 3))
+    # plt.subplot(121)
+    # plt.bar(lbl_axes, lat_axes)  # latency distribution
+    # plt.ylabel('latency')
+    # plt.subplot(122)
+    # plt.bar(lbl_axes, snr_axes)  # snr distribution
+    # plt.ylabel('snr')
+    # plt.show()
+
+    # Es7 lab4
 
 
 if __name__ == "__main__":
